1efbe9b4671de47e038bab124bba62e1b5f71ff2
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-06-09 16:22:36 +0430

    create stitches config file

---
+++ stitches.config.js
@@ -0,0 +1,32 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {},
+    space: {},
+    fontSizes: {},
+    fonts: {},
+    fontWeights: {},
+    lineHeights: {},
+    letterSpacings: {},
+    sizes: {},
+    borderWidths: {},
+    borderStyles: {},
+    radii: {},
+    shadows: {},
+    zIndices: {},
+    transitions: {},
+  },
+  utils: {
+    marginX: (value) => ({ marginLeft: value, marginRight: value }),
+  },
+});
