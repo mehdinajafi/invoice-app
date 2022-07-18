c7e6bce1f6c8fc40ff70f104af04a2cbc25a854d
Mehdi Najafi <dev.mehdinajafi@gmail.com>
2022-07-18 16:51:14 +0430

    change jest config for module name mapper

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,14 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
-    // '^@/components/(.*)$': '<rootDir>/components/$1',
-
-    // '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/data/(.*)$": "<rootDir>/data/$1",
+    "^@/store/(.*)$": "<rootDir>/store/$1",
+    "^@/utilities/(.*)$": "<rootDir>/utilities/$1",
+    "^@/types/(.*)$": "<rootDir>/types/$1",
+    "^@/public/(.*)$": "<rootDir>/public/$1",
+    "^@/images/[./a-zA-Z0-9$_-]+\\.svg$": "<rootDir>/__mocks__/SvgrMock.js",
     "stitches-config": "<rootDir>/stitches.config.js",
   },
   testEnvironment: "jest-environment-jsdom",
